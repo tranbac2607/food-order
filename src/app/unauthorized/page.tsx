636a48eb0a6d01b15235b6f6b42a0774cb48353e
Trần Văn Bắc <tranbac2607@gmail.com>
2025-03-15 10:47:38 +0700

    fix build

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -4,13 +4,10 @@ import { useEffect } from 'react';
 
 import EmptyState from '@/components/EmptyState';
 
-interface UnauthorizeStateProps {
-  error: Error;
-}
+const UnauthorizeState = () => {
+  const error = new Error('Unauthorized Access'); // Tạo lỗi mặc định nếu cần
 
-const UnauthorizeState: React.FC<UnauthorizeStateProps> = ({ error }) => {
   useEffect(() => {
-    // eslint-disable-next-line no-console
     console.error(error);
   }, [error]);
 
@@ -25,19 +22,3 @@ const UnauthorizeState: React.FC<UnauthorizeStateProps> = ({ error }) => {
 };
 
 export default UnauthorizeState;
-
-// import ErrorLayout from '@/layouts/ErrorLayout/ErrorLayout';
-
-// function ErrorPage() {
-//   return <h1>There was an error, let me check on that</h1>;
-// }
-
-// ErrorPage.getLayout = (page: any) => <ErrorLayout>{page}</ErrorLayout>;
-
-// export const getStaticProps = () => ({
-//   props: {
-//     title: '🕵🏼 - error',
-//   },
-// });
-
-// export default ErrorPage;
