67171927022a20c3ca2d8214e210229e7a8c7428
Trần Văn Bắc <tranbac2607@gmail.com>
2025-03-15 11:25:25 +0700

    add config site map

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://food-order-five-green.vercel.app/list-food',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
